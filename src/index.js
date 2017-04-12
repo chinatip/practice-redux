d72f724c049e1c610afa935222e0d701e5ebbaa0
chinatip <chinatip.v@ku.th>
2017-04-12 15:03:22 +0700

    Added simple store

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import { createStore } from 'redux'
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+       return state - 1;
+      default:
+        return state;
+  }
+}
+
+const store = createStore(counter);
+
+const render = () => {
+  document.body.innerText = store.getState();
+}
+
+store.subscribe(render);
+render();
+
+document.addEventListener('click', () => {
+  store.dispatch({type: 'INCREMENT'});
+});
