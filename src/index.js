0b28963bf833f625a40ad17dbed4aa787e40502d
chinatip <chinatip.v@ku.th>
2017-04-15 23:30:54 +0700

    Try deepFreeze and expect

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 
+var deepFreeze = require('deep-freeze');
+var expect = require('expect');
+
 const counter = (state = 0, action) => {
   switch (action.type) {
     case 'INCREMENT':
@@ -46,3 +49,57 @@ const render = () => {
 
 store.subscribe(render);
 render();
+
+const addCounter = (list) => {
+  return [...list, 0];
+};
+
+const removeCounter = (list, index) => {
+  return [
+    ...list.slice(0, index), 
+    ...list.slice(index + 1)
+    ];
+};
+
+const incrementCounter = (list,index) => {
+  return [
+    ...list.slice(0, index),
+    list[index] + 1,
+    ...list.slice(index + 1)
+  ];
+};
+
+const testAddCounter = () => {
+  const listBefore = [];
+  const listAfter = [0];
+
+  deepFreeze(listBefore);
+
+  expect(
+    addCounter(listBefore)
+  ).toEqual(listAfter);
+};
+
+const testRemoveCounter = () => {
+  const listBefore = [0, 10, 20];
+  const listAfter = [0, 20];
+
+  deepFreeze(listBefore);
+
+  expect(
+    removeCounter(listBefore, 1)
+  ).toEqual(listAfter);
+};
+
+const testIncrementCounter = () => {
+  const listBefore = [0, 10, 20];
+  const listAfter = [0, 11, 20];
+
+  expect(
+    incrementCounter(listBefore, 1)
+  ).toEqual(listAfter);
+}
+testAddCounter();
+testRemoveCounter();
+testIncrementCounter();
+console.log('All tests passed');
